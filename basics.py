c44a7b1d9421024512da3c2f0fa62eb73727c099
rasheed ahmad <rasheedahmad22333@gmail.com>
2021-03-18 13:08:50 +0500

    Variables
    Variables naming conventions

--- basics.py
+++ basics.py
@@ -14,3 +14,20 @@ False
 
 # printing to screen
 print("My name is",name,", I am",age, "years old and my height is",height)
+
+# ----------------------------------------- #
+
+# Variables
+# variables are dyanamically typed in Python
+age = 20 # int
+name = "Rasheed Jan" # string
+
+# a variable name cannot start with a number
+# 1name = # invalid variable name
+
+# varibles names are case sensitive
+name = "Rasheed Jan"
+NAME = "Rasheed Jan" # two different variables
+
+# ----------------------------------------- #
+
