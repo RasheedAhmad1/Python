f7a5b8dd368933131ff60463837579a1081f5ab4
rasheed ahmad <rasheedahmad22333@gmail.com>
2021-03-18 13:53:11 +0500

    Order of Operation
    Order of operation is added and defined

--- basics.py
+++ basics.py
@@ -65,3 +65,20 @@ exponent = x ** y
 print("x to the power of y is: ",exponent)
 
 # ----------------------------------------- #
+
+# Order of Operation
+# python mathematic instructions takes BEDMAS Arithmetic operation
+# B - Bracket
+# E - Exponent
+# D - Division
+# M - Multiplication
+# A - Addition
+# S - Subtraction
+
+x = 2
+y = 3
+answer = 5 - 3 * (x + y)** 2 # first x + y --> 5, then 5 square --> 25, then 25 multiplied by 3 --> 75 and then 5 - 75 --> -70
+print(answer)
+
+# ----------------------------------------- #
+
