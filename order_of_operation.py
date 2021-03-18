15e61393b5f7f2cf12a0cdb8974187653bfbc3c2
rasheed ahmad <rasheedahmad22333@gmail.com>
2021-03-18 12:27:47 +0500

    Order_of_operation.py Added
    Performed a simple arthmitic operation

---
+++ order_of_operation.py
@@ -0,0 +1,13 @@
+# Order of Operation
+# Python mathematic instructions takes BEDMAS Arithmetic operation
+# B - Brackets
+# E - Exponent
+# D - Division
+# M - Multiplication
+# A - Addition
+# S - Subtraction
+
+x = 2
+y = 3
+answer = 5 - 3 * (x + y)** 2 # first x + y --> 5, then 5 square --> 25, then 25 multiplied by 3 --> 75 and then 5 - 75 --> -70
+print(answer)
