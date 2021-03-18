3ca621e360a133e081d0b11c60533c39825b768f
rasheed ahmad <rasheedahmad22333@gmail.com>
2021-03-18 13:03:47 +0500

    Data Types
    Simple data types are defined

---
+++ basics.py
@@ -0,0 +1,16 @@
+# Data Types
+# int
+age = 24
+
+# float 
+height = 5.11
+
+# string
+name = "Rasheed Jan"
+
+# boolean
+True
+False 
+
+# printing to screen
+print("My name is",name,", I am",age, "years old and my height is",height)
