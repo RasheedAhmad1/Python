bf233b9cdb12980cc289cd53eb0e8d2069299729
Rasheed Ahmad <rasheedahmad22333@gmail.com>
2021-04-11 15:32:20 +0500

    Update Data_Types.py

--- Basics/Data_Types.py
+++ Basics/Data_Types.py
@@ -53,61 +53,10 @@ print(type(a))
 
 # ----------------------------------------- #
 
+
+
+# ----------------------------------------- #
+
 # Boolean
 True
 False 
-
-# ----------------------------------------- #
-
-
-# Taking input form the user
-name = input("Enter your name: ") # input always returns a string
-print("You entered:",name)
-
-# ----------------------------------------- #
-
-# Arithmetic Operators
-# + --> Addition
-x = 3
-y = 2
-sum = x + y
-print("The sum is: ",sum)
-
-# - --> Subtraction
-sub = x - y
-print("x minus y is: ",sub)
-
-# * --> Multiplication
-product = x * y
-print("The product is: ",product)
-
-# / --> Division
-division = x / y
-print("The division is: ",division)
-
-# % --> Modulus
-reminder = x % y
-print("The reminder is: ",reminder)
-
-# ** --> Exponent/Power
-exponent = x ** y
-print("x to the power of y is: ",exponent)
-
-# ----------------------------------------- #
-
-# Order of Operation
-# python mathematic instructions takes BEDMAS Arithmetic operation
-# B - Bracket
-# E - Exponent
-# D - Division
-# M - Multiplication
-# A - Addition
-# S - Subtraction
-
-x = 2
-y = 3
-answer = 5 - 3 * (x + y)** 2 # first x + y --> 5, then 5 square --> 25, then 25 multiplied by 3 --> 75 and then 5 - 75 --> -70
-print(answer)
-
-# ----------------------------------------- #
-
