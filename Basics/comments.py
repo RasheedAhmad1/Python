4eb3d241e824124cb26dbe6b910325022fe2616e
Rasheed Ahmad <rasheedahmad22333@gmail.com>
2021-04-11 14:43:51 +0500

    Create comments.py

---
+++ Basics/comments.py
@@ -0,0 +1,4 @@
+# Comments
+# comments are used to explain the code, to make the code more readable and to prevent execution when debugging the code
+# Python comments are represented by a hash at the begging of the code line
+# I am a single line comment
