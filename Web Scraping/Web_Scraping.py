f5d597258814f32e7839a178ca8313eed7ca5319
rasheed ahmad <rasheedahmad22333@gmail.com>
2021-04-14 15:47:24 +0500

    Added some script in Web_Scraping.py

---
+++ Web Scraping/Web_Scraping.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd 
+
+pages = []
+titles = []
+prices = []
+stars = []
+urls = []
+
+pages_to_scrap = 5
+
+# Get the raw data
+for i in range(1, pages_to_scrap+1):
+    page_url = ('http://books.toscrape.com/catalogue/page-{}.html').format(i)
+    pages.append(page_url)
+
+print(pages)
+
+# Parse the text
+for item in pages:
+    page = requests.get(item) # get all content from the 'item'
+    soup = BeautifulSoup(page.text, 'html.parser') # take all the text from 'page' parse it and return onto the 'soup'
+# print(soup.prettify()) # prettify() will give the actual indentation of the code also
+
+# Get text from all 'h3'
+for i in soup.findAll('h3'):
+    title = i.getText()
+    titles.append(title)    
+
+# Get text from all 'h3'
+for j in soup.findAll('p', class_='price_color'):
+    price = j.getText()
+    prices.append(price) 
+
+# Get the star ratings for each
+for k in soup.findAll('p', class_='star-rating'):
+    for m, n in k.attrs.items():
+        star = n[1]
+        stars.append(star)
+
+# Find URLs of all images
+divs = soup.findAll('div', class_='image_container')
+for thumb in divs:
+    tags = thumb.find('img', class_='thumbnail')
+    url = 'http://books.toscrape.com/'+str(tags['src'])
+    clean_urls = url.replace('../', '')
+    urls.append(clean_urls)
+
+# Gather all the data in a dictionary
+data = {'Titles':titles, 'Prices':prices, 'Star ratings':stars, 'URLs':urls}
+# print(data)
+
+# Create dataframe from the dictionary
+my_data = pd.DataFrame(data=data)
+# print(my_data)
+ 
+# Reset the default index(starting from 0) to 1
+my_data.index+=1
+print(my_data)
+
+# Export the data as Excel file
+# my_data.to_excel('C:/Users/HP/Desktop/Python/Web Scraping/Output.xlsx')
