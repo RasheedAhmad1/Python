85cbdf1602dc759a92caf89f5429a356994b7d55
Rasheed Ahmad <rasheedahmad22333@gmail.com>
2021-04-17 14:04:31 +0500

    Delete 01.Comments.py

--- Basics/01.Comments.py
+++
@@ -1,4 +0,0 @@
-# Comments
-# comments are used to explain the code, to make the code more readable and to prevent execution when debugging the code
-# Python comments are represented by a hash at the begging of the code line
-# I am a single line comment
